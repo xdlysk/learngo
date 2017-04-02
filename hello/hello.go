e98eb7d663e73398ef799eb26036c67ee3a8d070
xdlysk <xdlysk@live.cn>
2017-04-02 10:57:14 +0800

    init

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import 
+(
+	"fmt"
+	"github.com/xdlysk/stringutil"
+)
+import "github.com/golang/example/stringutil"
+
+func main() {
+	fmt.Printf(stringutil.Reverse("Hello, world.\n"))
+}
