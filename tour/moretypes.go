ef1e215d8221088b7071fa0c16e8afe5aa4b361a
xdlysk <xdlysk@live.cn>
2017-04-05 22:30:29 +0800

    more moretyoes

--- tour/moretypes.go
+++ tour/moretypes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func moretypesAll(){
@@ -19,7 +20,111 @@ func moretypesAll(){
 
 	arraySlice()
 
+	sliceLiterals()
+
+	sliceDefaults()
+
+	sliceLengthAndCapacity()
+
+	nilSlices()
+
+	makeArray()
+
+	slicesOfSlices()
+
 	fmt.Println("=================moretypes begin===================")
+}
+
+func slicesOfSlices(){
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+func makeArray(){
+	a := make([]int, 5)
+	fmt.Printf(" len=%d cap=%d %v\n", len(a), cap(a), a)
+
+	b := make([]int, 0, 5)
+	fmt.Printf("len=%d cap=%d %v\n", len(b), cap(b), b)
+
+	c := b[:2]
+	fmt.Printf("len=%d cap=%d %v\n", len(c), cap(c), c)
+
+	d := c[2:5]
+	fmt.Printf("len=%d cap=%d %v\n", len(d), cap(d), d)
+}
+
+func nilSlices(){
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+}
+
+func sliceLengthAndCapacity(){
+	s:=[]int{1,2,3,4,5,6}
+	fmt.Printf("len=%d cap=%d %v\n",len(s),cap(s),s)
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	fmt.Printf("len=%d cap=%d %v\n",len(s),cap(s),s)
+	// Extend its length.
+	s = s[:4]
+	fmt.Printf("len=%d cap=%d %v\n",len(s),cap(s),s)
+	// Drop its first two values.
+	s = s[2:]
+
+	fmt.Printf("len=%d cap=%d %v\n",len(s),cap(s),s)
+}
+
+func sliceDefaults(){
+	s:=[]int{1,2,3,4,5,6}
+
+	s = s[1:4]
+
+	fmt.Println(s)
+
+	s=s[:2]
+
+	fmt.Println(s)
+
+	s=s[1:]
+	fmt.Println(s)
+}
+
+func sliceLiterals(){
+	q:= []int{1,2,3,4,5,6}
+	fmt.Println(q)
+
+	r:=[]bool{true,false,true,false,true,false}
+	fmt.Println(r)
+
+	s:=[]struct{
+		i int
+		b bool
+	}{
+		{i:2,b:true},
+		{3,false},
+		{4,true},
+		{5,false},
+	}
+
+	fmt.Println(s)
 }
 
 func arraySlice(){
