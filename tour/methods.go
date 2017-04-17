4c6197249dc6b8e57343458831629de321ae669c
xdlysk <xdlysk@live.cn>
2017-04-17 23:42:03 +0800

    method more

--- tour/methods.go
+++ tour/methods.go
@@ -11,6 +11,19 @@ func methodsAll(){
 	v :=VertexM{3,4}
 	fmt.Println(v.Abs())
 
+	v.Scale(10);
+	fmt.Println(v.Abs())
+
+	f:= MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+
+	var a Abser
+	a = f
+	a = &v
+	//a = v
+
+	fmt.Println(a.Abs())
+
 	fmt.Println("=================methods end===================")
 }
 
@@ -21,4 +34,22 @@ type VertexM struct{
 //similar with .net extend method
 func(v VertexM) Abs() float64{
 	return math.Sqrt(v.X * v.X + v.Y*v.Y)
+}
+
+func(v *VertexM) Scale(f float64){
+	v.X = v.X*f
+	v.Y = v.Y*f
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64{
+	if f<0{
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Abser interface{
+	Abs() float64
 }
